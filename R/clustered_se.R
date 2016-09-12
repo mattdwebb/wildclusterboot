d0fe70d8cf2ec601385650b20965474b1f2b3742
Scott McNeil <scott@scottmcneil.ca>
2016-09-11 23:29:55 -0400

    Refactored clustered_se to allow for multiway

--- R/clustered_se.R
+++ R/clustered_se.R
@@ -1,36 +1,75 @@
 #' Takes an lm model and returns named vector of clustered standard errors
 #'
+#' @param data Dataframe containing both model data and clusterby variables
 #' @param model lm object for which clustered errors sought
 #' @param clusterby Vector of cluster indices corresponding to X variables
 #' @param G Total number of groups, should be specified for performance
 #' @param cluster_ids Vector of unique cluster_ids, should be specified for performance
 #' @return Named vector of clustered standard errors
 #' @export
-clustered_se <- function(model, clusterby, G = NULL, cluster_ids = NULL){
+clustered_se <- function(data, model, clusterby, G = NULL, cluster_ids = NULL){
 
   #Check if model is class lm
   if(class(model) != 'lm'){
     stop('Model variable must be lm class')
   }
 
+  #Check if supplied clusterby is a formula
+  if(class(clusterby) == 'formula'){
+
+    #extract variables from formula
+    form_vars <- all.vars(clusterby)
+
+    #Create combinations of group dimensions as well as concatenated names
+    comb_list <- lapply(X = 1:length(form_vars), FUN = combn, x = form_vars, simplify = FALSE)
+    comb_vars <- unlist(comb_list, recursive = FALSE)
+    comb_names <- lapply(X = comb_vars, FUN = paste0, collapse = '')
+    comb_n <- lapply(X = comb_vars, length)
+
+    #Create sub-dataframes for each combination of group dimensions
+    data_combs <- lapply(X = comb_vars, function(comb) data[comb])
+
+    #Create matrix of all group dimension combinations
+    clustervars <- lapply(X = data_combs, FUN = Reduce, f = paste0)
+    names(clustervars) <- comb_names
+
+  } else{
+
+    clustervars <- data[clusterby]
+    comb_n <- 1
+
+  }
+
   #If unique clusterids not specified, generate them
   if(is.null(cluster_ids)){
-    cluster_ids <- unique(clusterby)
+    cluster_ids <- lapply(X = clustervars, FUN = unique)
   }
 
   #If G is not specified, calculate it
   if(is.null(G)){
-    G <- length(cluster_ids)
+    G <- lapply(cluster_ids, length)
   }
 
   #Recover model matrix from lm model
   X <- model.matrix(model)
 
+  #Calculate 'bread' matrix
+  bread <- solve(crossprod(X))
+
   #Get residuals from lm model
   uhat <- resid(model)
 
+  #TODO: generate sandwich mat for each comb of cluster dimensions, then add subtract and extract SEs
+
+  sandwich_list <- mapply(FUN = calc_sandwich_mat, clustervars = clustervars, cluster_ids = cluster_ids, G = G, comb_n = comb_n,
+                          MoreArgs = list(X = X, bread = bread, uhat = uhat), SIMPLIFY = FALSE, USE.NAMES = FALSE)
+
   #Calculate SE and return
-  se <- calc_cluster_se(X = X, uhat = uhat, clusterby = clusterby, cluster_ids = cluster_ids, G = G)
+  sandwich <- Reduce('+', sandwich_list)
+
+  #Take diagonal of sandwich to get SE vector and return it
+  se <- sqrt(diag(sandwich))
+
   return(se)
 
 }
